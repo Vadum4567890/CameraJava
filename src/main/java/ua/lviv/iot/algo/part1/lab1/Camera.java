9cb8c7e6982feec88700f5ff527d3dae09811dd3
vadimyff <solpo789@gmail.com>
2023-06-25 00:33:06 +0300

    Add second lab files

--- src/main/java/ua/lviv/iot/algo/part1/lab1/Camera.java
+++ src/main/java/ua/lviv/iot/algo/part1/lab1/Camera.java
@@ -7,62 +7,11 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Camera {
+public abstract class Camera {
     private String model;
-    private String resolution;
-    private double zoom;
-    private String memoryCardType;
-    private int photosCount;
-    private static Camera instance = new Camera();
+    private String brand;
+    private String lens;
+    public abstract String takePhoto();
 
-    public static Camera getInstance() {
-        if (instance == null) {
-            instance = new Camera();
-        }
-        return instance;
-    }
-    private Camera(String model) {
-        this.model = model;
-    }
-    public static Camera getInstance(String model) {
-        if (instance == null) {
-            instance = new Camera(model);
-        }
-        return instance;
-    }
-    public void resetZoom() {
-        this.zoom = 1;
-    }
-    public void savePhoto() {
-        this.photosCount +=1;
-    }
-    public void eraseMemory(){
-        this.photosCount = 0;
-    }
-    public void changeSettings(String resolution, double zoom){
-        this.resolution = resolution;
-        this.zoom = zoom;
-    }
 
-    @Override
-    public String toString() {
-        return "Camera " +
-                "model='" + model + '\'' +
-                ", resolution='" + resolution + '\'' +
-                ", zoom=" + zoom +
-                ", memoryCardType='" + memoryCardType + '\'' +
-                ", photosCount=" + photosCount;
-    }
-    public static void main(String[] args) {
-        Camera[] cameras = {
-                new Camera("Canon", "1920x1080", 2.5, "SD", 0),
-                new Camera(),
-                getInstance(),
-                getInstance()
-        };
-
-        for (Camera camera : cameras) {
-            System.out.println(camera.toString());
-        }
-    }
 }
